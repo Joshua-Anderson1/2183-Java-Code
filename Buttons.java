e04201270ab83d2f50a41f4c065ae4a382c8f1e5
Joshua-Anderson1 <43221975+Joshua-Anderson1@users.noreply.github.com>
2019-01-31 16:13:43 -0600

    Add files via upload

---
+++ Buttons.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+class Buttons {
+    int ButtonA = 1;
+    int ButtonB = 2;
+    int ButtonX = 3;
+    int ButtonY = 4;
+    int Rt_Trigger;
+    int Lt_Trigger;
+    //added trigger values to fix the error, idk if we'll use them.
+}
---
+++ copilot.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+class Copilot extends Buttons {
+    Joystick Joystick = new Joystick(1);
+}
